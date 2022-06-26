3d3ad7fd9840bacdd60beddc4ce604059dae95d9
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2022-06-25 23:13:19 -0500

    Update _seek_compact_client_feed.py
    add normalize for now

--- yaqd_seek/_seek_compact_client_feed.py
+++ yaqd_seek/_seek_compact_client_feed.py
@@ -2,6 +2,7 @@ import numpy as np
 import yaqc
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib
 import time
 
 cam = yaqc.Client(39005)
@@ -12,7 +13,7 @@ sl =(slice(None, -1, None), slice(None, -2, None))
 fig = plt.figure()
 
 ax = plt.subplot(111)
-ax_im = plt.imshow(cam.get_measured()["img"][sl], origin="lower")
+ax_im = plt.imshow(cam.get_measured()["img"][sl], origin="lower", norm = matplotlib.colors.Normalize())
 
 def update_img(y):
     ax_im.set_data(y)
